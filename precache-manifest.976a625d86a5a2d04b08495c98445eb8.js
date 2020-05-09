3e524c0048ac6a8aec69d56e8ea33e75ffc8a1e0
brahim-001 <brahimdouch@gmail.com>
2020-05-09 19:48:31 +0200

    Updates

---
+++ precache-manifest.976a625d86a5a2d04b08495c98445eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171c0ad7cef85bdc22775537e96ed2a1",
+    "url": "/brahim-001/React-in-github-demo/index.html"
+  },
+  {
+    "revision": "3f2cfcc41f3c1b8aa724",
+    "url": "/brahim-001/React-in-github-demo/static/css/main.64584ac1.chunk.css"
+  },
+  {
+    "revision": "dd53c3f13ea3d5d3284e",
+    "url": "/brahim-001/React-in-github-demo/static/js/2.fa7aebf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brahim-001/React-in-github-demo/static/js/2.fa7aebf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f2cfcc41f3c1b8aa724",
+    "url": "/brahim-001/React-in-github-demo/static/js/main.61e75d8a.chunk.js"
+  },
+  {
+    "revision": "e1949f2c838259b70159",
+    "url": "/brahim-001/React-in-github-demo/static/js/runtime-main.c066dad1.js"
+  }
+]);
